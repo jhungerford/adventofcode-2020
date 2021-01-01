73853d724e137d9c1415b791321b3b8b27a1ef46
John Hungerford <jhungerford@gmail.com>
2021-01-01 15:14:35 -0700

    Day 24 part 2.

--- day24/src/lib.rs
+++ day24/src/lib.rs
@@ -20,6 +20,8 @@ pub struct Instruction {
 impl FromStr for Instruction {
     type Err = ParseErr;
 
+    /// Parses a string into an instruction.  An instruction is made up of
+    /// directions (se, sw, ne, nw, e, and w) without any separators.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let chars: Vec<char> = s.chars().collect();
 
@@ -73,11 +75,8 @@ pub fn load_instructions(filename: &str) -> Vec<Instruction> {
 /// Tile uses cube coordinates to identify a tile - see: https://www.redblobgames.com/grids/hexagons/
 #[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
 pub struct Tile {
-    /// NorthWest - SouthEast
     x: i32,
-    /// NorthEast - SouthWest
     y: i32,
-    /// East - West
     z: i32,
 }
 
@@ -86,11 +85,18 @@ impl Tile {
     fn origin() -> Tile {
         Tile { x: 0, y: 0, z: 0 }
     }
+
+    /// Returns the tiles around this tile.
+    fn neighbors(&self) -> Vec<Tile> {
+        [E, SE, SW, W, NW, NE].iter().map(|dir| *self + *dir).collect()
+    }
 }
 
 impl Add<Direction> for Tile {
     type Output = Tile;
 
+    /// Adds the given direction to this tile.
+    /// See: https://www.redblobgames.com/grids/hexagons#coordinates-cube
     fn add(self, dir: Direction) -> Self::Output {
         match dir {
             Direction::E => Tile {
@@ -134,12 +140,16 @@ pub struct Grid {
 
 impl Grid {
     /// Returns a new grid.
-    pub fn new() -> Grid {
-        Grid { black_tiles: HashSet::new() }
+    pub fn new(instructions: &Vec<Instruction>) -> Grid {
+        let mut grid = Grid { black_tiles: HashSet::new() };
+
+        grid.run_all(instructions);
+
+        grid
     }
 
     /// Runs all of the instructions, returning the modified grid.
-    pub fn run_all(&mut self, instructions: &Vec<Instruction>) -> &Self {
+    fn run_all(&mut self, instructions: &Vec<Instruction>) -> &Self {
         for instruction in instructions {
             self.run(instruction);
         }
@@ -149,14 +159,62 @@ impl Grid {
 
     /// Runs the given instruction, which flips a tile located by following the directions
     /// from the tile at 0,0,0 in this grid.  Returns this grid, which has been modified.
-    pub fn run(&mut self, instruction: &Instruction) -> &Self {
+    fn run(&mut self, instruction: &Instruction) -> &Self {
         let tile = instruction.directions.iter()
             .fold(Tile::origin(), |tile, direction| tile + *direction);
 
+        self.flip(tile);
+
+        self
+    }
+
+    /// Flips the given tile - black becomes white, white becomes black.
+    fn flip(&mut self, tile: Tile) {
         if self.black_tiles.contains(&tile) {
             self.black_tiles.remove(&tile);
         } else {
             self.black_tiles.insert(tile);
+        }
+    }
+
+    /// Flips tiles according to rules, returning this modified grid.
+    pub fn tick(&mut self) -> &Self {
+        // Tiles are flipped simultaneously based on the following rules:
+        // - Black tiles with zero or >2 adjacent black tiles are flipped to white.
+        // - White tiles with 2 adjacent black tiles are flipped to black.
+
+        // Consider the black tiles and their neighbors.
+        let consider: HashSet<Tile> = self.black_tiles.iter().cloned()
+            .chain(self.black_tiles.iter().flat_map(|tile| tile.neighbors()))
+            .collect();
+
+        // Figure out which ones to flip based on the rules.
+        let flip: Vec<Tile> = consider.into_iter().filter(|tile| {
+            let black_neighbors = tile.neighbors().into_iter()
+                .filter(|neighbor| self.black_tiles.contains(neighbor))
+                .count();
+
+            if self.black_tiles.contains(tile) {
+                // Currently a black tile - flip if 0 or >2 neighbors are black tiles.
+                black_neighbors == 0 || black_neighbors > 2
+            } else {
+                // Currently a white tile - flip if exactly 2 neighbors are black tiles.
+                black_neighbors == 2
+            }
+        }).collect();
+
+        // Flip the tiles.
+        for tile in flip {
+            self.flip(tile);
+        }
+
+        self
+    }
+
+    /// Flips tiles according to rules the given number of times, returning the final grid.
+    pub fn tick_times(&mut self, times: usize) -> &Self {
+        for _ in 0..times {
+            self.tick();
         }
 
         self
@@ -198,7 +256,8 @@ mod tests {
 
     #[test]
     fn run_instruction() {
-        let mut grid = Grid::new();
+        let instructions = vec![];
+        let mut grid = Grid::new(&instructions);
 
         grid.run(&"esenee".parse().unwrap());
         assert!(grid.black_tiles.contains(&Tile { x: 3, y: -3, z: 0 }));
@@ -210,6 +269,37 @@ mod tests {
     #[test]
     fn run_sample() {
         let instructions = load_instructions("sample.txt");
-        assert_eq!(10, Grid::new().run_all(&instructions).num_black());
+        assert_eq!(10, Grid::new(&instructions).num_black());
+    }
+
+    #[test]
+    fn tick_sample() {
+        let instructions = load_instructions("sample.txt");
+
+        let mut first_grid = Grid::new(&instructions);
+        assert_eq!(2208, first_grid.tick_times(100).num_black());
+
+        let mut grid = Grid::new(&instructions);
+
+        assert_eq!(15, grid.tick().num_black());
+        assert_eq!(12, grid.tick().num_black());
+        assert_eq!(25, grid.tick().num_black());
+        assert_eq!(14, grid.tick().num_black());
+        assert_eq!(23, grid.tick().num_black());
+        assert_eq!(28, grid.tick().num_black());
+        assert_eq!(41, grid.tick().num_black());
+        assert_eq!(37, grid.tick().num_black());
+        assert_eq!(49, grid.tick().num_black());
+        assert_eq!(37, grid.tick().num_black());
+
+        assert_eq!(132, grid.tick_times(10).num_black());
+        assert_eq!(259, grid.tick_times(10).num_black());
+        assert_eq!(406, grid.tick_times(10).num_black());
+        assert_eq!(566, grid.tick_times(10).num_black());
+        assert_eq!(788, grid.tick_times(10).num_black());
+        assert_eq!(1106, grid.tick_times(10).num_black());
+        assert_eq!(1373, grid.tick_times(10).num_black());
+        assert_eq!(1844, grid.tick_times(10).num_black());
+        assert_eq!(2208, grid.tick_times(10).num_black());
     }
 }
--- day24/src/main.rs
+++ day24/src/main.rs
@@ -3,6 +3,6 @@ use day24::{load_instructions, Grid};
 fn main() {
     let instructions = load_instructions("input.txt");
 
-    println!("Part 1: {}", Grid::new().run_all(&instructions).num_black());
-
+    println!("Part 1: {}", Grid::new(&instructions).num_black());
+    println!("Part 2: {}", Grid::new(&instructions).tick_times(100).num_black());
 }
